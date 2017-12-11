adc14d42cdf19f026cb94900e6d8267189fa96d0
Kayla Webb <kaylarwebb@gmail.com>
2017-12-11 15:36:13 -0500

    Added border.

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,8 +7,8 @@ export default function Contact(props) {
     //recieving props.data
    return (
         <li>
-            <span className='imgname'>
-            <img src={props.data.photo}/><span className='valign'>{props.data.name}</span></span>
+            <span>
+            <img src={props.data.photo}/></span><span className='valign'>{props.data.name}</span>
             <span className='address valign'> {props.data.address} </span>
         </li>
     )
