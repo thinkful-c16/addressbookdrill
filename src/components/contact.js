0f29bec91a3f634bf3bed94780a231bcc2e1313b
Kayla Webb <kaylarwebb@gmail.com>
2017-12-11 14:54:04 -0500

    Renders addresses.

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,10 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+    //recieving props.data
+   return (
+        <li><img src={props.data.photo}/>{props.data.name}{props.data.address}</li>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import AddressBook from './components/address-book';
 import './index.css';
 
+
+
 ReactDOM.render(
   <AddressBook />,
   document.getElementById('root')
